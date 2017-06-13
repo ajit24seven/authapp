d300724fd6b03f02e0d7b62a21ddccd152c36bd4
Ajit Kumar Giri <ajitkumargiri@Ajits-MacBook-Air.local>
2017-06-14 02:04:04 +0530

    connected to database

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const passport = require("passport");
 const mongoose = require("mongoose");
+const config = require("./config/database");
 
+//Database Connection
+mongoose.connect(config.database);
+mongoose.connection.on("connected", () => {
+    console.log("connected to the database " + config.database)
+});
+
+mongoose.connection.on("error", (err) => {
+    console.log("connected err " + err)
+});
+
+
+//Express initiated
 const app = express();
 
 const users = require("./routes/users");
@@ -14,6 +27,9 @@ const port = 3000;
 
 //Cors Middleware
 app.use(cors());
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, "public")));
 
 //Body Parser Middleware
 app.use(bodyParser.json());
